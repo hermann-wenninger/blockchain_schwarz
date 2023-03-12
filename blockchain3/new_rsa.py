223d0d566118c242733d679cf25d1a97b66d008d
hermann <hermann23@vivaldi.net>
2023-03-12 19:27:36 +0100

    key and keys

--- blockchain3/new_rsa.py
+++ blockchain3/new_rsa.py
@@ -2,31 +2,41 @@ from Crypto.PublicKey import RSA
 
 
 def create_keys():
+    participants = set()
     for i in range(10):
         key = RSA.generate(1024)
         private_key = key.export_key()
-        file_out = open(f"private-{i}.pem", "wb")
+        file_out = open(f"wallets/private-{i}.pem", "wb")
         file_out.write(private_key)
         file_out.close()
 
         public_key = key.publickey().export_key()
-        file_out = open(f"receiver-{i}.pem", "wb")
+        participants.add(public_key)
+        file_out = open(f"wallets/receiver-{i}.pem", "wb")
         file_out.write(public_key)
         file_out.close()
-
+        print('77777777777777777777',participants)
+    return participants
+create_keys()
 
 def create_set():
     list = []
     dict = {}
     for i in range(10):
-        with open(f"receiver-{i}.pem", 'rb')as f:
+        with open(f"wallets/receiver-{i}.pem", 'rb')as f:
             x = f.readlines()
             print('xxxxxxxxxxx',len(x))
             print(x.remove(x[0]),x.remove(x[-1]))
             dict[i] = x
-            list.append(x)
-    print(list)       
+            #list.append(x)
+    #print(list)       
     print(dict)
-    print('########',b"".join(dict[0]))
+    print('###########################',b"".join(dict[0]))
 
+    for i in range(len(dict)):
+        print(dict[i])
+        for j in dict[i]:
+            print(j)
+    for kw in dict:
+        print(kw, ":", dict[kw][-1])
 create_set()
